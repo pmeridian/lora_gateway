b6821b4e683af03a3a7003b459da1c7bdb91a749
Paolo Meridiani <paolo.meridiani@cern.ch>
2020-05-08 16:00:31 +0200

    moving to SF12

--- key_ThingSpeak.py
+++ key_ThingSpeak.py
@@ -15,6 +15,6 @@ field_association=[ (7,1) ]
 # [(6,1),(7,5)] means data from respectively sensor 6/7 will use starting field index of 1/5
 #field_association=[(6,1),(7,5)
 
-nomenclature_association=[ ("T1",0), ("T2",1), ("T3",2), ("T4",3), ("H1",4), ("H3",5), ("BT",6), ("IC",7) ]
+nomenclature_association=[ ("T1",0), ("T2",1), ("T3",2), ("T4",3), ("H1",4), ("H2",5), ("H3",6), ("IC",7) ]
 # ("TC",0) means that if nomemclature is "TC" then the offset for field index will be 0
 # nomenclature_association=[("TC",0),("HU",1),("LU",2),("CO2",3)]
